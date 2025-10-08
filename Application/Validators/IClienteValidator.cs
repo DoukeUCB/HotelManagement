5133c95d45b454d1b760d80d6a1c8516bd3079d6
Nicolette <nicolette.chalup@ucb.edu.bo>
2025-10-08 17:18:50 -0400

    Creamos Validadores del Cliente Implementa la lógica de validación, incluyendo la unicidad del email

---
+++ Application/Validators/IClienteValidator.cs
@@ -0,0 +1,11 @@
+using HotelManagement.DTOs;
+
+namespace HotelManagement.Aplicacion.Validators
+{
+    public interface IClienteValidator
+    {
+        Task ValidateCreateAsync(ClienteCreateDTO dto);
+        Task ValidateUpdateAsync(string id, ClienteUpdateDTO dto);
+        Task ValidateDeleteAsync(string id);
+    }
+}
